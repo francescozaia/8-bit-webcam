a86ef41da03c052b9ee69eabb5de153449269618
Francesco Zaia <francesco.zaia@gmail.com>
2014-02-02 18:00:17 +0000

    improved UI... a little

--- static/js/index.js
+++ static/js/index.js
@@ -1,96 +1,160 @@
-(function() {
-    var capturing = false;
-    var image = new Image();
-    var my_div = document.getElementById("imageContainer");
-    document.body.insertBefore(image, my_div);
 
-    var myc = document.createElement("canvas");
-    myc.width = 640;
-    myc.height = 480;
-    var ctx=myc.getContext("2d");
+;(function ( _app, undefined ) {
+    'use strict';
 
-    var myc2 = document.createElement("canvas");
-    myc2.width = 640;
-    myc2.height = 480;
-    var ctx2=myc2.getContext("2d");
-    ctx2.mozImageSmoothingEnabled = false;
-    ctx2.webkitImageSmoothingEnabled = false;
-    ctx2.imageSmoothingEnabled = false;
+    "use strict";
+
+    var _capturing = false,
+        _imageElement = new Image(),
+        _mainElement = document.getElementById("main");
+
+    var _imageCanvas = document.createElement("canvas"),
+        _imageCanvasContext = _imageCanvas.getContext("2d");
 
 
-    var size = 0.4;
+    var _pixelCanvas = document.createElement("canvas"),
+        _pixelCanvasContext = _pixelCanvas.getContext("2d");
 
-    camera.init({
-        mirror: true,
+    var _inputRGB = document.getElementsByName("rgb")[0],
+        _inputPixel = document.getElementsByName("pixel")[0];
 
-        onFrame: function(canvas) { //TODO clean allllllll
+    _app._checkedRGB = false;
+    _app._checkedPixel = false;
 
-            var imgData = canvas.getContext("2d").getImageData(0,0,640,480);
-
-            for (var i=0;i<imgData.data.length;i+=4) {
-                imgData.data[i+0] = _getBit(imgData.data[i+0]); //R
-                imgData.data[i+1] = _getBit(imgData.data[i+1]); //G
-                imgData.data[i+2] = _getBit(imgData.data[i+2]); //B
-            }
-
-            ctx.putImageData(imgData,0,0);
-
-            image.src = myc.toDataURL("image/png");
-
-            ctx2.drawImage(image, 0, 0, 160, 120);
-
-            ctx2.drawImage(myc2, 0, 0, 160, 120, 0, 0, myc2.width, myc2.height);
-
-            image.src = myc2.toDataURL("image/png");
-
-            function _getBit (_v) {
-                var _r = 0
-                if ( _v < 16 ) {
-                    _r = 0;
-                } else if ( _v < 48 ) {
-                    _r = 32;
-                } else if ( _v < 80 ) {
-                    _r = 64;
-                } else if ( _v < 112 ) {
-                    _r = 96;
-                } else if ( _v < 144 ) {
-                    _r = 128;
-                } else if ( _v < 176 ) {
-                    _r = 160;
-                } else if ( _v < 208 ) {
-                    _r = 192;
-                } else if ( _v < 240 ) {
-                    _r = 224;
-                } else {
-                    _r = 256;
-                }
-                return _r;
-            }
-
-        },
-
-        onSuccess: function() {
-            document.getElementById("info").style.display = "none";
-
-            capturing = true;
-            document.getElementById("pause").style.display = "block";
-            document.getElementById("pause").onclick = function() {
-                if (capturing) {
-                    camera.pause();
-                } else {
-                    camera.start();
-                }
-                capturing = !capturing;
-            };
-        },
-
-        onError: function(error) {
-            // TODO: log error
-        },
-
-        onNotSupported: function() {
-            document.getElementById("info").style.display = "none";
-            document.getElementById("notSupported").style.display = "block";
+    function _getBit(_v) {
+        var _ret = 0;
+        if ( _v < 16 ) {
+            _ret = 0;
+        } else if ( _v < 48 ) {
+            _ret = 32;
+        } else if ( _v < 80 ) {
+            _ret = 64;
+        } else if ( _v < 112 ) {
+            _ret = 96;
+        } else if ( _v < 144 ) {
+            _ret = 128;
+        } else if ( _v < 176 ) {
+            _ret = 160;
+        } else if ( _v < 208 ) {
+            _ret = 192;
+        } else if ( _v < 240 ) {
+            _ret = 224;
+        } else {
+            _ret = 256;
         }
-    });
-})();
+        return _ret;
+    }
+
+    function checkRGB() {
+        if (_inputRGB.checked) {
+            _app._checkedRGB = true;
+        } else {
+            _app._checkedRGB = false;
+        }
+    }
+
+    function checkPixel() {
+        if (_inputPixel.checked) {
+            _app._checkedPixel = true;
+        } else {
+            _app._checkedPixel = false;
+        }
+    }
+
+    /* private method */
+    function init() {
+
+        document.body.insertBefore(_imageElement, _mainElement);
+
+        _imageCanvas.width = 640;
+        _imageCanvas.height = 480;
+
+        _pixelCanvas.width = 640;
+        _pixelCanvas.height = 480;
+
+        _pixelCanvasContext.mozImageSmoothingEnabled = false;
+        _pixelCanvasContext.webkitImageSmoothingEnabled = false;
+        _pixelCanvasContext.imageSmoothingEnabled = false;
+
+
+
+
+        _inputRGB.onchange = checkRGB;
+        _inputPixel.onchange = checkPixel;
+
+        camera.init({
+
+            mirror: true,
+            width: 640,
+            height: 480,
+
+            onFrame: function(canvas) {
+
+                if (_app._checkedRGB) {
+
+                    var imgData = canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.height);
+
+                    // rendering 8-bit image
+
+                    for (var i=0;i<imgData.data.length;i+=4) {
+                        imgData.data[i+0] = _getBit(imgData.data[i+0]); //R
+                        imgData.data[i+1] = _getBit(imgData.data[i+1]); //G
+                        imgData.data[i+2] = _getBit(imgData.data[i+2]); //B
+                    }
+
+                    _imageCanvasContext.putImageData(imgData,0,0);
+
+                    _imageElement.src = _imageCanvas.toDataURL("image/png");
+                } else {
+                    _imageElement.src = canvas.toDataURL("image/png");
+                }
+
+                // pixelating
+
+                if (_app._checkedPixel) {
+                    _pixelCanvasContext.drawImage(_imageElement, 0, 0, 160, 120);
+
+                    _pixelCanvasContext.drawImage(_pixelCanvas, 0, 0, 160, 120, 0, 0, _pixelCanvas.width, _pixelCanvas.height);
+
+                    _imageElement.src = _pixelCanvas.toDataURL("image/png");
+                }
+
+
+
+            },
+
+            onSuccess: function() {
+                document.getElementById("info").style.display = "none";
+
+                _capturing = true;
+                document.getElementById("pause").style.display = "block";
+                document.getElementById("pause").onclick = function() {
+                    if (_capturing) {
+                        camera.pause();
+                    } else {
+                        camera.start();
+                    }
+                    _capturing = !_capturing;
+                };
+            },
+
+            onError: function(error) {
+                // TODO: log error
+            },
+
+            onNotSupported: function() {
+                document.getElementById("info").style.display = "none";
+                document.getElementById("notSupported").style.display = "block";
+            }
+
+        });
+    }
+
+    /* public method here used as "constructor" */
+    _app.initialize = function () {
+        init();
+    };
+
+}(window.app = window.app || {}));
+
+app.initialize();
